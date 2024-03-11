dd4dd1399c0e73e6a23ede3a3e9e495dc69f1e65
Rubén <rubcas178@gmail.com>
2024-03-11 14:12:02 +0100

    Inclusion de TodoCode y Paginación

--- src/main/java/com/example/CRUDStudents/controller/StudentController.java
+++ src/main/java/com/example/CRUDStudents/controller/StudentController.java
@@ -3,6 +3,11 @@ package com.example.CRUDStudents.controller;
 import com.example.CRUDStudents.Repository.IStudentRepository;
 import com.example.CRUDStudents.entity.Students;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,7 +29,7 @@ public class StudentController {
         return iStudentRepository.save(newstudent);
     }
 
-    @PatchMapping("/students/edit/{id}")
+    @PutMapping("/students/edit/{id}")
     public Students editStudent(@PathVariable Integer id, @RequestBody Students student){
         Optional<Students> studentsOptional = iStudentRepository.findById(id);
         if (studentsOptional.isPresent()){
@@ -47,5 +52,43 @@ public class StudentController {
         iStudentRepository.deleteById(id);
     }
 
+    @GetMapping("/hola")
+public String saludo(){
+        return "hola!";
+    }
+
+    @GetMapping("/holanombre/{nombre}/{edad}")
+    public String holaMundoNobre(@PathVariable String nombre,@PathVariable int edad){
+return  "Hola! "+nombre+ " Tu edad es :" + edad;
+    }
+
+@GetMapping("/students/page/{pagina}/{tamaño}")
+    public Page<Students> getStudents(@PathVariable int pagina,@PathVariable int tamaño){
+final Pageable pageable = PageRequest.of(pagina,tamaño, Sort.by(Sort.Direction.ASC,"name"));
+        return (Page<Students>) iStudentRepository.findAll(pageable);
+}
+
+    @GetMapping("/students/page")
+    public Page<Students> getStudens(){
+        final Pageable pageable = PageRequest.of(0,2);
+        return (Page<Students>) iStudentRepository.findAll(pageable);
+    }
+
+
+    @GetMapping ("/StudentsPageNombreAscendente")
+    public Page<Students> getAllStudentsPaginadosNombre(Pageable pageable){
+        // final Pageable pageable = PageRequest.of(0,5, Sort.by(Sort.Direction.ASC));
+        return iStudentRepository.findAll(pageable);
+    }
+
+    @GetMapping ("/StudentsPageVariable")
+    public Page<Students> getAllStudentsVariablePage(@PageableDefault()Pageable pageable){
+        return iStudentRepository.findAll(pageable);
+    }
+
+    @GetMapping ("/StudentsPageDefault")
+    public Page<Students> getAllStudentsDefaultPage(@PageableDefault(page = 0,size = 20)Pageable pageable){
+        return iStudentRepository.findAll(pageable);
+    }
 
 }
